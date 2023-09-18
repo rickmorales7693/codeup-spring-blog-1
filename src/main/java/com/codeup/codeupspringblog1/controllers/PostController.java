888a7485ade9bdada768b114736d99e3670f8e14
Rick Morales <rick.morales7693@gmail.com>
2023-09-18 08:54:13 -0500

    finished PostController, show and index html, Post.java

--- src/main/java/com/codeup/codeupspringblog1/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog1/controllers/PostController.java
@@ -1,33 +1,49 @@
 package com.codeup.codeupspringblog1.controllers;
 
 
+import com.codeup.codeupspringblog1.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
 
     @GetMapping("")
-    @ResponseBody
-    public String index() {
-        return "posts index page";
+//    @ResponseBody
+    public String index(Model model){
+        List<Post> postArr = new ArrayList<>();
+        Post newpost = new Post("Hello", "Hello World");
+        Post newpost2 = new Post("Codeup", "Hello Codeup");
+
+        postArr.add(newpost);
+        postArr.add(newpost2);
+
+        model.addAttribute("posts", postArr);
+
+        return "post/index";
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public String viewIndividualPost(@PathVariable long id) {
-        return "view an individual post";
+//    @ResponseBody
+    public String viewIndividualPost(@PathVariable long id, Model model) {
+        Post newpost = new Post("Hello", "Hello World");
+        model.addAttribute("post", newpost);
+        return "posts/show";
     }
 
     @GetMapping("/create")
-    @ResponseBody
+//    @ResponseBody
     public String showCreatePostView() {
         return "view the form for creating a post";
     }
 
     @PostMapping("/create")
-    @ResponseBody
+//    @ResponseBody
     public String createNewPost() {
         return "create a new post";
     }
---
+++ src/main/java/com/codeup/codeupspringblog1/models/Post.java
@@ -0,0 +1,22 @@
+package com.codeup.codeupspringblog1.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    private String title;
+    private String body;
+
+
+
+
+
+
+}
