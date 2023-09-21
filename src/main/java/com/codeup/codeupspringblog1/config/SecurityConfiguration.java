7d6b0da044ae9131ad1f0ceffb81bc1a5fb44718
Rick Morales <rick.morales7693@gmail.com>
2023-09-21 11:37:20 -0500

    In SecurityConfiguration.java switched out all the ads to posts

--- src/main/java/com/codeup/codeupspringblog1/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/codeupspringblog1/config/SecurityConfiguration.java
@@ -43,15 +43,15 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests((requests) -> requests
                         /* Pages that require authentication
                          * only authenticated users can create and edit ads */
-                        .requestMatchers("/ads/create", "/ads/*/edit").authenticated()
+                        .requestMatchers("/posts/create", "/posts/*/edit").authenticated()
                         /* Pages that do not require authentication
                          * anyone can visit the home page, register, login, and view ads */
-                        .requestMatchers("/", "/ads", "/ads/*", "/sign-up", "/login").permitAll()
+                        .requestMatchers("/", "/posts", "/posts/*", "/sign-up", "/login").permitAll()
                         // allow loading of static resources
                         .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
                 )
                 /* Login configuration */
-                .formLogin((login) -> login.loginPage("/login").defaultSuccessUrl("/ads"))
+                .formLogin((login) -> login.loginPage("/login").defaultSuccessUrl("/posts"))
                 /* Logout configuration */
                 .logout((logout) -> logout.logoutSuccessUrl("/"));
         return http.build();
